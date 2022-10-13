7cdb61927e590a623b6825b155651cdfc9efc800
Gautam-2002 <gautam2sethi@gmail.com>
2022-10-13 16:09:25 +0530

    react

---
+++ React/First/index.js
@@ -0,0 +1,13 @@
+// console.log(React);
+
+let div = document.querySelector('#root')
+
+// let ele = React.createElement('h1',null,"heya duniya")
+// ReactDOM.render(ele,div);
+
+
+// let ele = React.createElement('div',null,React.createElement('h3',null,"Good afternoon sir"));
+// ReactDOM.render(ele,div)
+
+
+//npx create-react-app test
