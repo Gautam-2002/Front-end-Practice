5e23cd941cac2e3123ce97e155471a76add61088
Gautam-2002 <gautam2sethi@gmail.com>
2023-05-07 18:00:14 +0530

    jsx-babel

--- React/First/index.js
+++ React/First/index.js
@@ -1,8 +1,8 @@
 // console.log(React);
 
-let div = document.querySelector('#root')
+// let div = document.querySelector('#root')
 
-// let ele = React.createElement('h1',null,"heya duniya")
+// let ele = React.createElement('h1',null,"using js create element")
 // ReactDOM.render(ele,div);
 
 
@@ -14,3 +14,22 @@ let div = document.querySelector('#root')
 
 // babel - it's a compiler which helps to compile jsx
 // webpack - a bundeler
+
+// using jsx :
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<h1>this is jsx</h1>,)
+
+
+// react 17 vs. react 18 :
+
+// Before
+// import { render } from 'react-dom';
+// const container = document.getElementById('app');
+// render(<App tab="home" />, container);
+
+// After
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('app');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
